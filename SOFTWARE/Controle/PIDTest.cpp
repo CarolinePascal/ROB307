29625728d4d1438255ffb201e0b38a40027e9072
CarolinePascal <caroline8.pascal@gmail.com>
2019-11-26 20:08:45 +0100

    Fixing unread stream issue - Ready for HLS

--- SOFTWARE/Controle/PIDTest.cpp
+++ SOFTWARE/Controle/PIDTest.cpp
@@ -4,27 +4,41 @@
 #include <iostream>
 #include <fstream>
 
+//PID paramters : Kp,Ki,Kd,scale,min,max,dt
+static float PIDValues[7] = {1, 1, 1, 1, -2, 10, 0.1};
+
 int main()
 {
+    //Initialise arrays
+
+    //Will be updated by the current thread
     //previous error, cumulated error
     float previous[2];
     //command, error
     float PIDOutput[2];
-    //Kp,Ki,Kd,scale,min,max,dt
-    static float PIDValues[7] = {1, 1, 1, 1, -2, 10, 0.1};
 
+    //Can be technically modified by another thread !
     volatile float objective = 5;
     volatile float current = 0;
 
-    hls::stream<intSdCh> inputStream;
-    hls::stream<intSdCh> outputStream;
+    hls::stream<intSdCh> inputStream("inputStream");
+    hls::stream<intSdCh> outputStream("outputStream");
 
     int counter = 0;
 
+    //Main loop
     while (counter < 10)
     {
-        /************** send data ******************/
+        printf("Current = ");
+        printf("%f ", current);
+        printf("Objective = ");
+        printf("%f ", objective);
 
+        counter++;
+        //current = computeCurrent();
+        //objective = computeObjective();
+
+        //Send data to the input stream
         intSdCh aValue;
         aValue.data = current;
         aValue.last = 0;
@@ -33,7 +47,7 @@ int main()
         aValue.user = 0;
         aValue.id = 0;
         aValue.dest = 0;
-        outputStream.write(aValue);
+        inputStream.write(aValue);
 
         aValue.data = objective;
         aValue.last = 0;
@@ -42,16 +56,21 @@ int main()
         aValue.user = 0;
         aValue.id = 0;
         aValue.dest = 0;
-        outputStream.write(aValue);
+        inputStream.write(aValue);
 
         PID(inputStream, outputStream);
 
-        printf("res={");
-        intSdCh valOut;
-        outputStream.read(valOut);
-        current = valOut.data;
-        outputStream.read(valOut);
-        printf("%d", (int)valOut.data);
+        printf("Result={");
+        for (int i = 0; i < 2; i++)
+        {
+            intSdCh valOut;
+            outputStream.read(valOut);
+            if (i == 0)
+                current = valOut.data;
+            printf("%f", (float)valOut.data);
+            if (i == 0)
+                printf(",");
+        }
         printf("} \n");
     }
     return 0;
